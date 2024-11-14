0fd76719480222a81be0d8e39259ac9c229ec925
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-11-14 17:56:32 +0900

    Server

---
+++ web/app.mjs
@@ -0,0 +1,18 @@
+import express from "express";
+import { search, SafeSearchType } from "duck-duck-scrape";
+
+const app = express()
+const port = 3000
+app.use(express.json());
+
+app.post('/search', async(req, res) => {
+  const searchBody = req.body.search;
+  const searchResults = await search(searchBody, {
+    safeSearch: SafeSearchType.STRICT
+  });
+  res.send(searchResults);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
