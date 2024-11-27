7665ff066223ece618eebc3194a7ce8410052247
Hansol Son <60804796+HansolSon1113@users.noreply.github.com>
2024-11-27 18:39:38 +0900

    Prompt

--- app/src/OpenAI.js
+++ app/src/OpenAI.js
@@ -9,19 +9,18 @@ import searchTool from "./tools/search"
 const tools = [timeTool, mbtiQuestionTool, mbtiSaveTool, searchTool]
 
 const model = new ChatOpenAI({
-    model: "gpt-4o-mini",
+    model: "gpt-4o",
     apiKey: process.env.REACT_APP_OPENAI_API_KEY,   //Value should be in /.env
 });
 
 const prompt =
     `
-        You are an MBTI assessment assistant that engages users in friendly, everyday conversation.
-        You must use MBTI_Save tool on each chat, use id and index -1 if there is nothing to save.
-        1. You will receive some basic information about the user through chat before beginning the assessment.
-        2. Gather MBTI questions using the MBTI_Question tool at the start, but do not reveal that you are conducting a test or mention MBTI explicitly.
-        3. Hide the original MBTI questions and instead guide the conversation naturally to understand the user’s preferences and behaviors.
-        5. When the MBTI_Save tool returns a value of 47, respond with "FINISH_CHAT" to end the conversation.
-        6. Keep a relaxed, friendly tone, and avoid structured questioning to create a smooth, enjoyable experience.
+        당신은 사용자와 일상적인 대화를 통해 MBTI 성격 유형을 분석하는 어시스턴트입니다.  
+        모든 대화 이후에는 MBTI_Save 툴을 사용해 데이터를 저장합니다(저장할 내용이 없으면 -1, -1을 사용하세요).
+        직접 질문을 하기보다는 대화 속에서 사용자의 성향을 파악해야 합니다.
+        사용자의 말투, 표현, 감정 상태 등을 분석하여 성격 특성을 추출하세요.    
+        초기 메시지에서 사용자의 기본 정보(나이, 직업, 국적, 흥미 등)이 주어집니다.
+        초기 메시지를 응답할땐 MBTI_Question 툴을 사용하여 검사 항목들을 불러와야 합니다.
     `;
 
 const checkpointer = new MemorySaver();
@@ -50,7 +49,7 @@ async function Invoke(message) {
 
     console.log(result);
     return result.messages[result.messages.length - 1].content;
-    
+
     //Debug
     // const langGraphStream = await agent.stream(
     //     { messages: [{ role: "user", content: message }] },
